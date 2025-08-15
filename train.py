323759dae7a60ec73e8a18ee48b5f2ce2e17e520
xcheng-tsinghua <786682191@qq.com>
2025-08-15 11:30:40 +0800

    change

--- train.py
+++ train.py
@@ -26,6 +26,7 @@ def parse_args():
     parser.add_argument('--decay_rate', type=float, default=1e-4, help='decay rate')
 
     parser.add_argument('--n_point', type=int, default=2000, help='Point Number')
+    parser.add_argument('--is_load_weight', default='True', choices=['True', 'False'], type=str)
 
     parser.add_argument('--local', default='False', choices=['True', 'False'], type=str)
     parser.add_argument('--root_sever', type=str, default=rf'/opt/data/private/data_set/pcd_cstnet2')
@@ -62,12 +63,15 @@ def main(args):
     # loading model
     classifier = PointNet2Regression()
 
-    model_savepth = 'model_trained/' + save_str + '.pth'
-    try:
-        classifier.load_state_dict(torch.load(model_savepth))
-        print('training from exist model: ' + model_savepth)
-    except:
-        print('no existing model, training from scratch')
+    if eval(args.is_load_weight):
+        model_savepth = 'model_trained/' + save_str + '.pth'
+        try:
+            classifier.load_state_dict(torch.load(model_savepth))
+            print('training from exist model: ' + model_savepth)
+        except:
+            print('no existing model, training from scratch')
+    else:
+        print('does not load weight, training from scratch')
 
     classifier = classifier.cuda()
 
