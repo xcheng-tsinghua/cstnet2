8adac1bb9c55e1d7a5cc62cf9114c34ed8eaf5cc
xcheng-tsinghua <786682191@qq.com>
2025-11-05 17:47:33 +0800

    change

--- models_3rd/pointnet2.py
+++ models_3rd/pointnet2.py
@@ -253,6 +253,75 @@ class PointNetSetAbstraction(nn.Module):
         return new_xyz, new_points
 
 
+class PointNetFeaturePropagation(nn.Module):
+    def __init__(self, in_channel, mlp): # fp1: in_channel=150, mlp=[128, 128]
+        super(PointNetFeaturePropagation, self).__init__()
+        self.mlp_convs = nn.ModuleList()
+        self.mlp_bns = nn.ModuleList()
+        last_channel = in_channel
+        for out_channel in mlp:
+            self.mlp_convs.append(nn.Conv1d(last_channel, out_channel, 1))
+            self.mlp_bns.append(nn.BatchNorm1d(out_channel))
+            last_channel = out_channel
+
+    def forward(self, xyz1, xyz2, points1, points2):
+        """
+        xyz2为从xyz1中采样获得的点坐标，points1, points2 为对应属性
+        对于xyz1中的某个点(center)，找到xyz2中与之最近的3个点(nears)，将nears的特征进行加权和，得到center的插值特征
+        nears中第i个点(near_i)特征的权重为 [1/d(near_i)]/sum(k=1->3)[1/d(near_k)]
+        d(near_i)为 center到near_i的距离，即距离越近，权重越大
+        之后拼接points1与xyz中每个点的更新属性，再利用MLP对每个点的特征单独进行处理
+
+        Input:
+            xyz1: input points position data, [B, C, N]
+            xyz2: sampled input points position data, [B, C, S]
+            points1: input points data, [B, D, N]
+            points2: sampled points data, [B, D, S]
+        Return:
+            new_points: upsampled points data, [B, D', N]
+        """
+        xyz1 = xyz1.permute(0, 2, 1)
+        xyz2 = xyz2.permute(0, 2, 1)
+        points2 = points2.permute(0, 2, 1)
+
+        B, N, C = xyz1.shape
+        _, S, _ = xyz2.shape
+
+        if S == 1:
+            interpolated_points = points2.repeat(1, N, 1)
+        else:
+            # 计算xyz1中的每个点到xyz2中每个点的距离 xyz1:[bs, N, 3], xyz2:[bs, S, 3], return: [bs, N, S]
+            dists = square_distance(xyz1, xyz2)
+
+            # 计算每个初始点到采样点距离最近的3个点，sort 默认升序排列
+            dists, idx = dists.sort(dim=-1)
+            dists, idx = dists[:, :, :3], idx[:, :, :3]  # [B, N, 3]
+
+            # 最近距离的每行求倒数
+            dist_recip = 1.0 / (dists + 1e-8)
+            norm = torch.sum(dist_recip, dim=2, keepdim=True)
+
+            # 求倒数后每行中每个数除以该行之和
+            weight = dist_recip / norm  # ->[B, N, 3]
+
+            # index_points(points2, idx): 为原始点集中的每个点找到采样点集中与之最近的3个三个点的特征 -> [B, N, 3, D]
+            interpolated_points = torch.sum(index_points(points2, idx) * weight.view(B, N, 3, 1), dim=2)
+
+        if points1 is not None:
+            # skip link concatenation
+            points1 = points1.permute(0, 2, 1)
+            new_points = torch.cat([points1, interpolated_points], dim=-1)
+        else:
+            new_points = interpolated_points
+
+        new_points = new_points.permute(0, 2, 1)
+        # 使用MLP对每个点的特征单独进行处理
+        for i, conv in enumerate(self.mlp_convs):
+            bn = self.mlp_bns[i]
+            new_points = F.relu(bn(conv(new_points)))
+        return new_points
+
+
 class PointNet2Cls(nn.Module):
     # num_class = 40，normal_channel = false
     def __init__(self, num_class: int, fea_channel=0):
@@ -336,6 +405,51 @@ class PointNet2Reg(nn.Module):
         res = self.fc3(res)
 
         return res
+
+
+class PointNet2Seg(nn.Module):
+    def __init__(self, num_classes, normal_channel=False):
+        super().__init__()
+        if normal_channel:
+            additional_channel = 3
+        else:
+            additional_channel = 0
+        self.normal_channel = normal_channel
+        self.sa1 = PointNetSetAbstraction(npoint=512, radius=0.2, nsample=32, in_channel=6+additional_channel, mlp=[64, 64, 128], group_all=False)
+        self.sa2 = PointNetSetAbstraction(npoint=128, radius=0.4, nsample=64, in_channel=128 + 3, mlp=[128, 128, 256], group_all=False)
+        self.sa3 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, in_channel=256 + 3, mlp=[256, 512, 1024], group_all=True)
+        self.fp3 = PointNetFeaturePropagation(in_channel=1280, mlp=[256, 256])  # in_chanell = points2_chanell + points1_channel
+        self.fp2 = PointNetFeaturePropagation(in_channel=384, mlp=[256, 128])
+        self.fp1 = PointNetFeaturePropagation(in_channel=128+16+6+additional_channel, mlp=[128, 128, 128])
+        self.conv1 = nn.Conv1d(128, 128, 1)
+        self.bn1 = nn.BatchNorm1d(128)
+        self.drop1 = nn.Dropout(0.5)
+        self.conv2 = nn.Conv1d(128, num_classes, 1)
+
+    def forward(self, xyz, cls_label):
+        # Set Abstraction layers
+        B,C,N = xyz.shape
+        if self.normal_channel:
+            l0_points = xyz
+            l0_xyz = xyz[:,:3,:]
+        else:
+            l0_points = xyz
+            l0_xyz = xyz
+        l1_xyz, l1_points = self.sa1(l0_xyz, l0_points)
+        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
+        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
+        # Feature Propagation layers
+        l2_points = self.fp3(l2_xyz, l3_xyz, l2_points, l3_points)
+        l1_points = self.fp2(l1_xyz, l2_xyz, l1_points, l2_points)
+        cls_label_one_hot = cls_label.view(B,16,1).repeat(1,1,N)
+        l0_points = self.fp1(l0_xyz, l1_xyz, torch.cat([cls_label_one_hot,l0_xyz,l0_points],1), l1_points)
+        # FC layers
+        feat = F.relu(self.bn1(self.conv1(l0_points)))
+        x = self.drop1(feat)
+        x = self.conv2(x)
+        x = F.log_softmax(x, dim=1)
+        x = x.permute(0, 2, 1)
+        return x, l3_points
 
 
 if __name__ == '__main__':
