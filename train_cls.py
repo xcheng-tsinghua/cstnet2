5bd47bc9b83391830e692f5b5fb9c319c93bf207
xcheng-tsinghua <786682191@qq.com>
2026-06-07 10:28:37 +0800

    change

--- train_cls.py
+++ train_cls.py
@@ -52,7 +52,7 @@ def parse_args():
     parser.add_argument("--root_local", type=str, default=r"D:\document\DeepLearning\DataSet\pcd_cstnet2\Param20K_Extend")
 
     parser.add_argument("--constraint_source", choices=["stage1", "gt"], default="stage1")
-    parser.add_argument("--stage1_model", choices=["pointnet2", "pointnet", "attn_3dgcn"], default="pointnet2")
+    parser.add_argument("--stage1_model", choices=["pointnet2", "pointnet", "attn_3dgcn"], default="attn_3dgcn")
     parser.add_argument("--stage1_ckpt", type=str, default=r"model_trained/attn_3dgcn_pmt_prim_cluster.pth")
     parser.add_argument("--stage1_cluster_bandwidth", type=float, default=0.35)
     parser.add_argument("--stage1_use_gt_normals", default="False", choices=["True", "False"])
