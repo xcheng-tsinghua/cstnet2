00ec7ecc946d14f1165ef8ad6f9897f8454b34de
xcheng-tsinghua <786682191@qq.com>
2026-09-08 17:55:01 +0800

    change

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ if __name__ == '__main__':
     # remove_norm_(r'/opt/data/private/data_set/pcd_cstnet2/mfcad_pcd')
     # remove_norm_(r'/opt/data/private/data_set/pcd_cstnet2/Param20K_pcd')
     convert_stage1_txt_to_h5(
-        input_dir=[r'/opt/data/private/data_set/pcd_cstnet2/mfcad_pcd', r'/opt/data/private/data_set/pcd_cstnet2/tmcad_pcd'],
-        output_dir=r'/opt/data/private/data_set/pcd_cstnet2/stage1_all')
+        input_dir=r'/opt/data/private/data_set/pcd_cstnet2/mfcad_pcd',
+        output_dir=r'/opt/data/private/data_set/pcd_cstnet2/stage1_small2_h5')
+
+
     pass
 
 
